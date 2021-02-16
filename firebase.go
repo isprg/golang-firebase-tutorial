abbf47770c3c4e1335748327c1a31b42434a230f
RedBullet2000 <63525348+RedBullet2000@users.noreply.github.com>
2021-02-16 10:50:28 +0900

    add some comments

--- firebase.go
+++ firebase.go
@@ -10,21 +10,30 @@ import (
 )
 
 func main() {
+	// アプリの設定ファイル等の指定
 	opt := option.WithCredentialsFile("firebase_token.json")
 	config := &firebase.Config{
 		DatabaseURL: "https://magiot.firebaseio.com/",
 	}
+
+	// アプリの初期化
 	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
+
+	// クライアント作成
 	client, err := app.Database(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var flags map[string]int
+
+	// リファレンスの取得
 	ref := client.NewRef("data/flag")
+
+	// データの取得
 	err = ref.Get(context.Background(), &flags)
 	if err != nil {
 		log.Fatal(err)
